b83b5c7799a35672cb0bc44bcc9acc2f2f5dbc9b
Kellen Bolger <bolger.kellen@gmail.com>
2021-07-26 23:11:24 -0400

    added google auth and protected api route

---
+++ src/pages/api/user.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { getSession } from "next-auth/client"
+
+export default async (req, res) => {
+  const session = await getSession({ req })
+
+  if (session) {
+    res.send(session.user)
+  } else {
+    res.send({
+      error: "You must be signed in to view the protected content",
+    })
+  }
+}
